72a013d974b6d1da673be10b9fa48e3895dd934b
caines1989 <6423872+caines1989@users.noreply.github.com>
2021-08-25 19:49:08 +0100

    pushing all

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './App.css';
-import HomeScreen from './HomeScreen';
+import HomeScreen from './pages/HomeScreen';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginScreen from './pages/LoginScreen';
 
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-     <HomeScreen /> 
+
+
+     <Router>
+       {!user ? (
+         <LoginScreen />
+       ): (
+        <Switch>
+          <Route exact path="/">
+          <HomeScreen /> 
+          </Route>
+        </Switch>
+       )}
+
+
+    </Router>
+
+
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCMru9l5DwopN8t9T4YOdt3r_uikezm_FA",
     authDomain: "netflix-clone-f5939.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "778492399718",
     appId: "1:778492399718:web:14acee0fe1d825f6d75da7"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default { db };
